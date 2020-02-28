d48aecafbf9dbc762d6ae1d35f35832368212902
Adriaan Koster <info@adriaankoster.nl>
2020-02-28 21:53:05 +0100

    Outputs total number of words

--- src/main/java/nl/nuggit/countit/CountIt.java
+++ src/main/java/nl/nuggit/countit/CountIt.java
@@ -3,7 +3,9 @@ package nl.nuggit.countit;
 public class CountIt {
 
     public String parse(String document) {
-        return document.replaceAll("\\d+", "");
+        String output = document.replaceAll("\\d+", "");
+        String[] words = output.split("\\s+");
+        return String.format("Number of words: %s", words.length);
     }
 
 }
--- src/test/java/nl/nuggit/countit/CountItTest.java
+++ src/test/java/nl/nuggit/countit/CountItTest.java
@@ -1,6 +1,7 @@
 package nl.nuggit.countit;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -10,11 +11,21 @@ public class CountItTest {
 
     @Test
     public void testNumbersAreIgnored() {
-        String document = "test 123";
+        String document = "The big brown fox number 4 ";
 
         String output = countIt.parse(document);
 
-        assertFalse(output.contains("123"));
+        assertFalse(output.contains("4"));
+    }
+
+    @Test
+    public void testOutputsTotalNumberOfWords() {
+        String document = "The big brown fox number 4 ";
+
+        String output = countIt.parse(document);
+
+        String expectedStart = "Number of words: 5";
+        assertTrue(output.startsWith(expectedStart));
 
     }
 
